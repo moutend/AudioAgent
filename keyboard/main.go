795d06c6fa5297f7e4f4a59fb466c6f252140ef1
Yoshiyuki Koyanagi <moutend@gmail.com>
2019-12-02 09:22:43 +0900

    Chore

--- keyboard/main.go
+++ keyboard/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"mime"
 	"net/http"
 
 	"github.com/nsf/termbox-go"
 )
+
+type command struct {
+	Type  int         `json:"type"`
+	Value interface{} `json:"value"`
+}
+
+type commandRequest struct {
+	Commands []command `json:"commands"`
+}
 
 func main() {
 	contentType := mime.TypeByExtension(".json")
@@ -34,7 +46,23 @@ func main() {
 				go http.Post("http://192.168.1.112:4000/v1/engine/quit", contentType, nil)
 			case termbox.KeySpace:
 			default:
-				go http.Post("http://192.168.1.112:4000/v1/engine/feed", contentType, nil)
+				cmd := commandRequest{
+					Commands: []command{
+						{
+							Type:  1,
+							Value: 0,
+						},
+						{
+							Type:  3,
+							Value: "Hello, 世界",
+						},
+					},
+				}
+				data, err := json.Marshal(cmd)
+				if err != nil {
+					log.Fatal(err)
+				}
+				go http.Post("http://192.168.1.112:4000/v1/engine/command", contentType, bytes.NewBuffer(data))
 				go http.Post("http://192.168.1.112:4000/sound/feed", contentType, nil)
 			}
 		}
