b5e272195b12556cc50cd5b5e9b244d4b2453744
Yoshiyuki Koyanagi <moutend@gmail.com>
2019-12-31 11:45:01 +0900

    Chore

--- cmd/AudioNode/main.go
+++ cmd/AudioNode/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/hex"
+	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/signal"
+	"os/user"
+	"path/filepath"
+	"time"
 
 	"github.com/moutend/AudioNode/pkg/app"
 )
@@ -15,6 +21,34 @@ func main() {
 }
 
 func run(args []string) error {
+	rand.Seed(time.Now().Unix())
+	p := make([]byte, 16)
+
+	if _, err := rand.Read(p); err != nil {
+		return err
+	}
+
+	u, err := user.Current()
+
+	if err != nil {
+		return err
+	}
+
+	fileName := fmt.Sprintf("AudioNode-%s.txt", hex.EncodeToString(p))
+	outputPath := filepath.Join(u.HomeDir, "AppData", "Roaming", "ScreenReaderX", "SystemLog", fileName)
+	os.MkdirAll(filepath.Dir(outputPath), 0755)
+
+	file, err := os.OpenFile(outputPath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	log.SetFlags(log.Llongfile | log.LUTC)
+	log.SetOutput(file)
+
 	app := app.New()
 
 	if err := app.Setup(); err != nil {
